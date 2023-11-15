152696706869eb59c9d828b8e13aa70958e71ed6
AnastasiaDro <adrogunova@gmail.com>
2023-11-15 15:03:07 +0300

    created tokensDataModel

---
+++ app/src/main/java/com/cerebus/tokens/domain/models/TokensData.kt
@@ -0,0 +1,11 @@
+package com.cerebus.tokens.domain.models
+
+data class TokensData(
+    var number: Int,
+    var color: Int,
+    var minTokensNumber: Int = 1,
+    var maxTokensNumber: Int = 10,
+    var checkedTokensNumber: Int = 0,
+) {
+
+}
