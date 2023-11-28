a8bd8f326c96e4933ecf5b6167ad9947172f6988
AnastasiaDro <adrogunova@gmail.com>
2023-11-28 15:22:55 +0300

    adding navcontroller

--- app/src/main/java/com/cerebus/tokens/presentation/MainActivity.kt
+++ app/src/main/java/com/cerebus/tokens/presentation/MainActivity.kt
@@ -2,6 +2,9 @@ package com.cerebus.tokens.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.cerebus.tokens.R
 import com.cerebus.tokens.navigator.Destinations
 import com.cerebus.tokens.navigator.Modifiers
@@ -10,13 +13,18 @@ import com.cerebus.tokens.navigator.NavigatorImpl
 
 
 class MainActivity : AppCompatActivity() {
-    private val navigator: Navigator = NavigatorImpl()
-    fun getNavigator() = navigator
+//    private val navigator: Navigator = NavigatorImpl()
+//    fun getNavigator() = navigator
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        navigator.navigate(Destinations.TOKENS_FRAGMENT, this, Modifiers.REPLACE)
+       // navigator.navigate(Destinations.TOKENS_FRAGMENT, this, Modifiers.REPLACE)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_container) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
     }
 }
