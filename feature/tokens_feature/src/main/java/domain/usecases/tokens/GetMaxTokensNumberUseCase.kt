f57766ce9f13a1fdd3a493d48a20263f960f2685
AnastasiaDro <adrogunova@gmail.com>
2024-01-17 22:10:00 +0300

    deleted package prefixes

--- feature/tokens_feature/src/main/java/domain/usecases/tokens/GetMaxTokensNumberUseCase.kt
+++ feature/tokens_feature/src/main/java/domain/usecases/tokens/GetMaxTokensNumberUseCase.kt
@@ -2,7 +2,7 @@ package domain.usecases.tokens
 
 import domain.repository.TokensRepository
 
-class GetMaxTokensNumberUseCase(private val tokensRepository: domain.repository.TokensRepository) {
+class GetMaxTokensNumberUseCase(private val tokensRepository: TokensRepository) {
 
     fun execute() = tokensRepository.getMaxTokensNumber()
 }
