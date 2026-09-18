0c76dba4c381e083dab0cf9c89f14a3dad502e02
cerebus <adevdrogunova@gmail.com>
2026-09-19 00:18:55 +0300

    test: correct tokens instrumentation package expectation
    Align the legacy template assertion with the existing feature namespace. Verified all 82 Android tests on API 34, debug Kotlin compilation and JVM test tasks.

--- feature/tokens_feature/impl/src/androidTest/java/com/cerebus/tokens/presentation/ExampleInstrumentedTest.kt
+++ feature/tokens_feature/impl/src/androidTest/java/com/cerebus/tokens/presentation/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.cerebus.tokens.presentation", appContext.packageName)
+        assertEquals("com.cerebus.tokens.feature.tokens_feature.test", appContext.packageName)
     }
 }
